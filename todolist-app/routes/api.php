7aaa374069da2e58e4c48a26a8e0b84ae9257035
DiltaFebiana <febianadilta@gmail.com>
2022-04-25 23:39:52 +0700

    Praktikum 2

--- todolist-app/routes/api.php
+++ todolist-app/routes/api.php
@@ -23,3 +23,4 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function (){
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+Route::apiResource('/todos', TodoController::class);
